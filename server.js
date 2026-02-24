ed4f2db7c24b2708554d745a948351c1c8fd5fb6
Guray Topagac <guraytopagac0@gmail.com>
2026-02-24 19:21:25 +0300

    ES sayfası geçici olarak eklendi

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const public = path.join(__dirname, "public")
 app.use("/", express.static(path.join(public, "indexPage")));
 app.use("/games", express.static(path.join(public, "gamesPage")));
 app.use("/games/DTL", express.static(path.join(public, "gamesPage", "gameSource", "DTL")));
+app.use("/games/ES", express.static(path.join(public, "gamesPage", "gameSource", "ES")));
 app.use("/sign-up", express.static(path.join(public, "signUpPage")));
 app.use("/upload", express.static(path.join(public, "uploadPage")));
 
